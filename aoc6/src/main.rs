cc2fe84c4c52173493613468466b29242edd3568
Rob Donnelly <rfdonnelly@gmail.com>
2019-01-13 17:04:49 -0800

    Day 6 Part 1
    Need to filter extreme POIs

--- aoc6/src/main.rs
+++ aoc6/src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{self, Read};
+use std::collections::HashMap;
 
 fn main() -> io::Result<()> {
     let mut input = String::new();
@@ -14,7 +15,7 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 struct Point {
     x: i32,
     y: i32,
@@ -23,6 +24,14 @@ struct Point {
 impl Point {
     fn new(x: i32, y: i32) -> Point {
         Point { x, y }
+    }
+
+    fn from_str(s: &str) -> Point {
+        let tokens: Vec<&str> = s.split(", ").collect();
+        let x = tokens.first().unwrap().parse::<i32>().unwrap();
+        let y = tokens.last().unwrap().parse::<i32>().unwrap();
+
+        Point::new(x, y)
     }
 
     fn distance(&self, b: Point) -> i32 {
@@ -38,34 +47,155 @@ struct Rect {
     bottom: i32,
 }
 
-fn part1(lines: &[&str]) -> i32 {
+#[derive(Debug, PartialEq)]
+struct Grid {
+    nearest_neighbors: Vec<Vec<Option<usize>>>,
+    pois: Vec<Point>,
+    border: Rect,
+}
+
+struct MinResult {
+    multimatch: bool,
+    min: i32,
+    min_index: usize,
+}
+
+impl MinResult {
+    fn new() -> MinResult {
+        MinResult {
+            multimatch: false,
+            min: std::i32::MAX,
+            min_index: std::usize::MAX,
+        }
+    }
+}
+
+impl Grid {
+    fn new(pois: Vec<Point>) -> Grid {
+        let border = border(&pois);
+        let nearest_neighbors = Grid::nearest_neighbors(&pois, &border);
+
+        Grid {
+            nearest_neighbors,
+            pois,
+            border,
+        }
+    }
+
+    fn nearest_neighbors(pois: &[Point], border: &Rect) -> Vec<Vec<Option<usize>>> {
+        let mut cells = vec![vec![None; border.bottom as usize]; border.right as usize];
+
+        for x in 0..cells.len() {
+            for y in 0..cells[x].len() {
+                let cell_position = Point::new(x as i32, y as i32);
+
+                let min = pois
+                    .iter()
+                    .enumerate()
+                    .fold(MinResult::new(), |mut result, (index, poi)| {
+                        let distance = cell_position.distance(*poi);
+
+                        if distance < result.min {
+                            result.min = distance;
+                            result.min_index = index;
+                            result.multimatch = false;
+                        } else if distance == result.min {
+                            result.multimatch = true;
+                        }
+
+                        result
+                    });
+
+                if !min.multimatch {
+                    cells[x][y] = Some(min.min_index);
+                }
+            }
+        }
+
+        cells
+    }
+
+    fn areas(&self) -> HashMap<usize, u32> {
+        let mut areas: HashMap<usize, u32> = HashMap::new();
+
+        for row in self.nearest_neighbors.iter() {
+            for cell in row {
+                if let Some(poi_index) = cell {
+                    areas.entry(*poi_index)
+                        .and_modify(|e| *e += 1)
+                        .or_insert(1);
+                }
+            }
+        }
+
+        areas
+    }
+
+    // TODO: Need to filter infinite areas (i.e. the four POIs closests to the
+    // corners)
+    fn max_area(&self) -> u32 {
+        self.areas()
+            .iter().max_by_key(|kv| kv.1)
+            .unwrap()
+            .1
+            .clone()
+    }
+}
+
+fn parse_lines(lines: &[&str]) -> Vec<Point> {
+    lines
+        .iter()
+        .map(|line| Point::from_str(line))
+        .collect()
+}
+
+fn part1(lines: &[&str]) -> u32 {
+    let pois = parse_lines(lines);
+
+    Grid::new(pois).max_area()
 }
 
 fn part2(lines: &[&str]) -> i32 {
+    0
 }
 
-fn extremes(points: &[Point]) -> Rect {
+fn border(pois: &[Point]) -> Rect {
     Rect {
-        left: points.map(|point| point.x).min().unwrap(),
-        right: points.map(|point| point.x).max().unwrap(),
-        top: points.map(|point| point.y).min().unwrap(),
-        bottom: points.map(|point| point.y).max().unwrap(),
+        left: pois.iter().map(|point| point.x).min().unwrap(),
+        right: pois.iter().map(|point| point.x).max().unwrap(),
+        top: pois.iter().map(|point| point.y).min().unwrap(),
+        bottom: pois.iter().map(|point| point.y).max().unwrap(),
     }
 }
 
 #[cfg(test)]
-module tests {
+mod tests {
     use super::*;
 
     #[test]
     fn part1() {
-        let points = vec![
-            Point::new(1, 1),
-            Point::new(1, 6),
-            Point::new(8, 3),
-            Point::new(3, 4),
-            Point::new(5, 5),
-            Point::new(8, 9),
+        let lines = vec![
+            "1, 1",
+            "1, 6",
+            "8, 3",
+            "3, 4",
+            "5, 5",
+            "8, 9",
         ];
+
+        let pois = parse_lines(&lines);
+
+        let grid = Grid::new(pois.clone());
+        let areas = grid.areas();
+        let max_area_poi = areas.iter().max_by_key(|kv| kv.1).unwrap();
+
+        println!("grid:{:?}", grid);
+        println!("areas:{:?}", areas);
+        println!("max_area_poi_index:{:?}", max_area_poi);
+        println!("max_area_poi_location:{:?}", pois[*max_area_poi.0]);
+
+        assert_eq!(grid.max_area(), 17);
+
+        assert_eq!(super::part1(&lines), 17);
     }
 }
