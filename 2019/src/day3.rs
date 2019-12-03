965cabd515acf16bd99756cdc10dfead20bb97a9
Rob Donnelly <rfdonnelly@gmail.com>
2019-12-03 14:29:54 -0800

    Add day3 part1

---
+++ 2019/src/day3.rs
@@ -0,0 +1,249 @@
+use std::fs;
+use std::io;
+
+pub(crate) fn main() -> io::Result<()> {
+    let input = fs::read_to_string("input/3")?;
+    let wires = parse_input(&input);
+
+    println!("day3::part1: {}", part1(&wires));
+    println!("day3::part2: {}", part2(&wires));
+
+    Ok(())
+}
+
+fn parse_input(s: &str) -> Vec<Wire> {
+    s
+        .lines()
+        .map(parse_line)
+        .collect()
+}
+
+fn part1(wires: &Vec<Wire>) -> i32 {
+    let wires: Vec<Vec<Segment>> = wires
+        .iter()
+        .map(wire_to_segments)
+        .collect();
+
+    intersections(&wires[0], &wires[1])
+        .iter()
+        .map(|p| p.manhattan_distance())
+        .min()
+        .unwrap()
+}
+
+fn part2(wires: &Vec<Wire>) -> u32 {
+    0
+}
+
+#[derive(Copy, Clone, Debug)]
+enum GridVector {
+    X(i32),
+    Y(i32),
+}
+
+type Wire = Vec<GridVector>;
+
+impl GridVector {
+    fn from_str(s: &str) -> Self {
+        let direction = s.chars().next().unwrap();
+        let magnitude = s[1 as usize ..].parse::<i32>().unwrap();
+
+        match direction {
+            'L' => Self::X(-magnitude),
+            'R' => Self::X(magnitude),
+            'U' => Self::Y(-magnitude),
+            'D' => Self::Y(magnitude),
+            _ => panic!(),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    fn new(x: i32, y: i32) -> Self {
+        Point {x, y}
+    }
+
+    fn manhattan_distance(&self) -> i32 {
+        self.x.abs() + self.y.abs()
+    }
+
+    fn is_origin(&self) -> bool {
+        self.x == 0 && self.y == 0
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+struct Segment(Point, Point);
+
+fn wire_to_segments(wire: &Wire) -> Vec<Segment> {
+    let mut p1 = Point::new(0, 0);
+    let mut segments = Vec::new();
+
+    for vector in wire {
+        let p2 =
+            match vector {
+                GridVector::X(magnitude) => {
+                    Point::new(p1.x + magnitude, p1.y)
+                }
+                GridVector::Y(magnitude) => {
+                    Point::new(p1.x, p1.y + magnitude)
+                }
+            };
+
+        segments.push(Segment(p1, p2));
+
+        p1 = p2;
+    }
+
+    segments
+}
+
+fn intersections(a_s: &[Segment], b_s: &[Segment]) -> Vec<Point> {
+    let mut intersections = Vec::new();
+
+    for a in a_s {
+        for b in b_s {
+            if let Some(intersection) = intersection(*a, *b) {
+                if !intersection.is_origin() {
+                    intersections.push(intersection);
+                }
+            }
+        }
+    }
+
+    intersections
+}
+
+fn intersection(a: Segment, b: Segment) -> Option<Point> {
+    if a.0.x == a.1.x && b.0.x == b.1.x {
+        // Parallel in the x dimension
+        // Assumes parallel lines do not overlap
+        None
+    } else if a.0.y == a.1.y && b.0.y == b.1.y {
+        // Parallel in the y dimension
+        // Assumes parallel lines do not overlap
+        None
+    } else {
+        // Normalize so that a always runs in the x dimension and b always runs in the y dimension
+        let (a, b) =
+            if a.0.x != a.1.x {
+                // Segment a runs in the x dimension, b runs in the y dimension
+                (a, b)
+            } else {
+                // Segment a runs in the y dimension, b runs in the x dimension
+                (b, a)
+            };
+
+        let c = Point::new(b.0.x, a.0.y);
+
+        if ((c.y >= b.0.y && c.y <= b.1.y) || (c.y >= b.1.y && c.y <= b.0.y)) &&
+           ((c.x >= a.0.x && c.x <= a.1.x) || (c.x >= a.1.x && c.x <= a.0.x))
+        {
+            Some(c)
+        } else {
+            None
+        }
+    }
+}
+
+fn parse_line(line: &str) -> Wire {
+    line
+        .split(",")
+        .map(GridVector::from_str)
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    impl Segment {
+        fn new(x0: i32, y0: i32, x1: i32, y1: i32) -> Segment {
+            Segment(
+                Point {x: x0, y: y0},
+                Point {x: x1, y: y1},
+            )
+        }
+    }
+
+    #[test]
+    fn test_intersection() {
+        assert_eq!(
+            intersection(
+                Segment::new(1, 0, 1, 3),
+                Segment::new(0, 1, 2, 1),
+            ),
+            Some(Point::new(1, 1))
+        );
+    }
+
+    #[test]
+    fn test_intersection_parallel_x() {
+        assert_eq!(
+            intersection(
+                Segment::new(2, 0, 2, 3),
+                Segment::new(1, 0, 1, 3),
+            ),
+            None
+        );
+    }
+
+    #[test]
+    fn test_intersection_parallel_y() {
+        // Parallel in y dimension
+        assert_eq!(
+            intersection(
+                Segment::new(0, 2, 2, 2),
+                Segment::new(0, 1, 2, 1),
+            ),
+            None
+        );
+    }
+
+    #[test]
+    fn test_intersection_short() {
+        assert_eq!(
+            intersection(
+                Segment::new(1, 0, 1, 3),
+                Segment::new(2, 1, 4, 1),
+            ),
+            None
+        );
+    }
+
+    #[test]
+    fn test_part1_example1() {
+        let input =
+            "R8,U5,L5,D3\n\
+             U7,R6,D4,L4";
+
+        let wires = parse_input(input);
+        assert_eq!(part1(&wires), 6);
+    }
+
+    #[test]
+    fn test_part1_example2() {
+        let input =
+            "R75,D30,R83,U83,L12,D49,R71,U7,L72\n\
+             U62,R66,U55,R34,D71,R55,D58,R83";
+
+        let wires = parse_input(input);
+        assert_eq!(part1(&wires), 159);
+    }
+
+    #[test]
+    fn test_part1_example3() {
+        let input =
+            "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\n\
+            U98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
+
+        let wires = parse_input(input);
+        assert_eq!(part1(&wires), 135);
+    }
+}
--- 2019/src/main.rs
+++ 2019/src/main.rs
@@ -1,11 +1,13 @@
 mod day1;
 mod day2;
+mod day3;
 
 use std::io;
 
 fn main() -> io::Result<()> {
     day1::main()?;
     day2::main()?;
+    day3::main()?;
 
     Ok(())
 }
