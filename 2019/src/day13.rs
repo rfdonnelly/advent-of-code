546580074120498a1c30c0ffff6c3a1ba2c529d8
Rob Donnelly <rfdonnelly@gmail.com>
2019-12-13 08:36:04 -0800

    Add day13 part1

---
+++ 2019/src/day13.rs
@@ -0,0 +1,122 @@
+use std::collections::HashMap;
+use std::fs;
+use std::io;
+
+use crate::computer::{
+    Computer,
+    Program,
+};
+use crate::lib::parse_input;
+
+pub(crate) fn main() -> io::Result<()> {
+    let (part1, part2) = day13();
+
+    println!("day13::part1: {}", part1);
+    println!("day13::part2: {}", part2);
+
+    Ok(())
+}
+
+fn day13() -> (usize, i64) {
+    let input = fs::read_to_string("input/13").unwrap();
+
+    let program: Program =
+        parse_input(&input, Program::from)
+        .into_iter()
+        .next()
+        .unwrap();
+
+    (part1(&program), part2(&program))
+}
+
+fn part1(program: &Program) -> usize {
+    let result =
+        Computer::new(program.clone(), vec![])
+            .run()
+            .unwrap();
+
+    let instructions = instructions_from_outputs(&result.outputs);
+    let board = board_from_instructions(&instructions);
+
+    board
+        .values()
+        .filter(|&&value| value == Tile::Block)
+        .count()
+}
+
+fn part2(program: &Program) -> i64 {
+    0
+}
+
+#[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
+struct Point {
+    x: i64,
+    y: i64,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+enum Tile {
+    Empty,
+    Wall,
+    Block,
+    Paddle,
+    Ball,
+}
+
+#[derive(Copy, Clone, Debug)]
+struct Instruction {
+    p: Point,
+    tile: Tile,
+}
+
+impl From<i64> for Tile {
+    fn from(i: i64) -> Self {
+        match i {
+            0 => Self::Empty,
+            1 => Self::Wall,
+            2 => Self::Block,
+            3 => Self::Paddle,
+            4 => Self::Ball,
+            _ => panic!(),
+        }
+    }
+}
+
+impl Instruction {
+    fn new(x: i64, y: i64, tile: i64) -> Self {
+        Instruction {
+            p: Point {x, y},
+            tile: Tile::from(tile),
+        }
+    }
+}
+
+fn instructions_from_outputs(outputs: &[i64]) -> Vec<Instruction> {
+    outputs
+        .chunks(3)
+        .map(|chunk| Instruction::new(chunk[0], chunk[1], chunk[2]))
+        .collect()
+}
+
+fn board_from_instructions(instructions: &[Instruction]) -> HashMap<Point, Tile> {
+    let mut map = HashMap::new();
+
+    for instruction in instructions {
+        match instruction.tile {
+            Tile::Empty => map.remove(&instruction.p),
+            _ => map.insert(instruction.p, instruction.tile),
+        };
+    }
+
+    map
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_day13() {
+        assert_eq!(day13(), (363, 0))
+    }
+}
--- 2019/src/main.rs
+++ 2019/src/main.rs
@@ -8,6 +8,7 @@ mod day7;
 mod day8;
 mod day9;
 mod day10;
+mod day13;
 
 mod computer;
 mod lib;
@@ -25,6 +26,7 @@ fn main() -> io::Result<()> {
     day8::main()?;
     day9::main()?;
     day10::main()?;
+    day13::main()?;
 
     Ok(())
 }
