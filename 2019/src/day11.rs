3796a5085c5f0bbc37293f857f3ebb7b519185ef
Rob Donnelly <rfdonnelly@gmail.com>
2019-12-14 13:58:13 -0800

    Add day11 part1

---
+++ 2019/src/day11.rs
@@ -0,0 +1,198 @@
+use std::collections::HashMap;
+use std::fs;
+use std::io;
+use std::ops::AddAssign;
+
+use crate::computer::{
+    Computer,
+    Program,
+    State as RunState,
+};
+use crate::lib::parse_input;
+
+pub(crate) fn main() -> io::Result<()> {
+    let (part1, part2) = day11();
+
+    println!("day11::part1: {}", part1);
+    println!("day11::part2: {}", part2);
+
+    Ok(())
+}
+
+fn day11() -> (usize, i64) {
+    let input = fs::read_to_string("input/11").unwrap();
+
+    let program: Program =
+        parse_input(&input, Program::from)
+        .into_iter()
+        .next()
+        .unwrap();
+
+    (part1(program.clone()), part2(&program))
+}
+
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+struct Point {
+    x: i64,
+    y: i64,
+}
+
+impl Point {
+    fn new(x: i64, y: i64) -> Self {
+        Self {x, y}
+    }
+}
+
+impl AddAssign<Direction> for Point {
+    fn add_assign(&mut self, other: Direction) {
+        match other {
+            Direction::Up => self.y += 1,
+            Direction::Down => self.y -= 1,
+            Direction::Left => self.x -= 1,
+            Direction::Right => self.x += 1,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Debug)]
+enum Turn {
+    Left,
+    Right,
+}
+
+impl From<i64> for Turn {
+    fn from(i: i64) -> Self {
+        match i {
+            0 => Self::Left,
+            1 => Self::Right,
+            _ => panic!(),
+        }
+    }
+}
+
+impl AddAssign<Turn> for Direction {
+    fn add_assign(&mut self, other: Turn) {
+        *self = match self {
+            Self::Up => {
+                match other {
+                    Turn::Left => Self::Left,
+                    Turn::Right => Self::Right,
+                }
+            }
+            Self::Left => {
+                match other {
+                    Turn::Left => Self::Down,
+                    Turn::Right => Self::Up,
+                }
+            }
+            Self::Down => {
+                match other {
+                    Turn::Left => Self::Right,
+                    Turn::Right => Self::Left,
+                }
+            }
+            Self::Right => {
+                match other {
+                    Turn::Left => Self::Up,
+                    Turn::Right => Self::Down,
+                }
+            }
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+enum Color {
+    Black,
+    White,
+}
+
+impl From<i64> for Color {
+    fn from(i: i64) -> Self {
+        match i {
+            0 => Self::Black,
+            1 => Self::White,
+            _ => panic!(),
+        }
+    }
+}
+
+impl Into<i64> for Color {
+    fn into(self) -> i64 {
+        match self {
+            Self::Black => 0,
+            Self::White => 1,
+        }
+    }
+}
+
+struct RobotState {
+    direction: Direction,
+    location: Point,
+    map: HashMap<Point, Color>,
+}
+
+impl RobotState {
+    fn new() -> Self {
+        Self {
+            direction: Direction::Up,
+            location: Point::new(0, 0),
+            map: HashMap::new(),
+        }
+    }
+
+    fn update(&mut self, color: Color, turn: Turn) {
+        self.map.insert(self.location, color);
+        self.direction += turn;
+        self.location += self.direction;
+    }
+
+    fn current_color(&self) -> Color {
+        *self.map
+            .get(&self.location)
+            .unwrap_or(&Color::Black)
+    }
+}
+
+fn part1(program: Program) -> usize {
+    let mut state = RobotState::new();
+    let mut computer = Computer::new(program, vec![]);
+
+    loop {
+        computer.push_input(state.current_color().into());
+        let result = computer.run().unwrap();
+
+        match result.state {
+            RunState::Halt => {
+                return state.map.len();
+            }
+            RunState::WaitForInput => {
+                let color = result.outputs[0];
+                let turn = result.outputs[1];
+                state.update(color.into(), turn.into());
+            }
+        }
+    }
+}
+
+fn part2(program: &Program) -> i64 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_day11() {
+        assert_eq!(day11(), (1785, 1))
+    }
+}
--- 2019/src/main.rs
+++ 2019/src/main.rs
@@ -8,6 +8,7 @@ mod day7;
 mod day8;
 mod day9;
 mod day10;
+mod day11;
 mod day13;
 mod day14;
 
@@ -27,6 +28,7 @@ fn main() -> io::Result<()> {
     day8::main()?;
     day9::main()?;
     day10::main()?;
+    day11::main()?;
     day13::main()?;
     day14::main()?;
 
