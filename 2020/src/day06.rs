9509bd51f32b8f018b3254166eff64dc778f7013
Rob Donnelly <rfdonnelly@gmail.com>
2020-12-07 10:31:22 -0800

    refactor: simplify

--- 2020/src/day06.rs
+++ 2020/src/day06.rs
@@ -3,15 +3,15 @@ use std::collections::HashSet;
 use anyhow::Result;
 
 pub fn day(day: usize, input: &str) -> Result<()> {
-    let data = parse_input(input)?;
-    println!("day{:02}::part1: {}", day, part1(&data)?);
-    println!("day{:02}::part2: {}", day, part2(&data)?);
+    println!("day{:02}::part1: {}", day, part1(input)?);
+    println!("day{:02}::part2: {}", day, part2(input)?);
 
     Ok(())
 }
 
-fn part1(groups: &[&str]) -> Result<usize> {
-    let count = groups.iter()
+fn part1(input: &str) -> Result<usize> {
+    let count = input
+        .split("\n\n")
         .map(|group| {
             let mut set = HashSet::new();
             for c in group.chars() {
@@ -25,12 +25,9 @@ fn part1(groups: &[&str]) -> Result<usize> {
     Ok(count)
 }
 
-fn parse_input<'a>(input: &'a str) -> Result<Vec<&'a str>> {
-    Ok(input.split("\n\n").collect())
-}
-
-fn part2(groups: &[&str]) -> Result<u32> {
-    let count = groups.iter()
+fn part2(input: &str) -> Result<u32> {
+    let count = input
+        .split("\n\n")
         .map(|group|
              group
                 .lines()
@@ -79,17 +76,13 @@ mod test {
         input
     }
 
-    fn data() -> Vec<&'static str> {
-        super::parse_input(input()).unwrap()
-    }
-
     #[test]
     fn part1() {
-        assert_eq!(super::part1(&data()).unwrap(), 11);
+        assert_eq!(super::part1(input()).unwrap(), 11);
     }
 
     #[test]
     fn part2() {
-        assert_eq!(super::part2(&data()).unwrap(), 6);
+        assert_eq!(super::part2(input()).unwrap(), 6);
     }
 }
