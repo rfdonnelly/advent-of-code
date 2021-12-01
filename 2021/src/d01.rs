5f5da9154e223bbc7df5446a3657607e322df8ec
Rob Donnelly <rfdonnelly@gmail.com>
2021-12-01 12:03:29 -0800

    Add 2021 d01

---
+++ 2021/src/d01.rs
@@ -0,0 +1,77 @@
+use crate::input;
+
+use itertools::Itertools;
+
+pub fn run() {
+    let input = input(1);
+    println!("d01p1: {}", d01p1(&input));
+    println!("d01p2: {}", d01p2(&input));
+}
+
+fn d01p1(input: &str) -> usize {
+    input
+        .lines()
+        .map(str::parse::<u32>)
+        .map(Result::unwrap)
+        .tuple_windows()
+        .filter(|(a, b)| b > a)
+        .count()
+}
+
+fn d01p2(input: &str) -> usize {
+    input
+        .lines()
+        .map(str::parse::<u32>)
+        .map(Result::unwrap)
+        .tuple_windows()
+        .map(|(a, b, c)| a + b + c)
+        .tuple_windows()
+        .filter(|(a, b)| b > a)
+        .count()
+}
+
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+
+    #[test]
+    fn d01p1() {
+        let input = indoc!{"
+            199
+            200
+            208
+            210
+            200
+            207
+            240
+            269
+            260
+            263
+        "};
+        assert_eq!(super::d01p1(input), 7);
+
+        let input = super::input(1);
+        assert_eq!(super::d01p1(&input), 1462);
+    }
+
+    #[test]
+    fn d01p2() {
+        let input = indoc!{"
+            199
+            200
+            208
+            210
+            200
+            207
+            240
+            269
+            260
+            263
+        "};
+        assert_eq!(super::d01p2(input), 5);
+
+        let input = super::input(1);
+        assert_eq!(super::d01p2(&input), 1497);
+    }
+}
+
---
+++ 2021/src/main.rs
@@ -0,0 +1,20 @@
+use std::fs;
+use std::path::{Path, PathBuf};
+
+mod d01;
+
+fn main() {
+    d01::run()
+}
+
+fn load_file<P>(path: P) -> String
+where
+    P: AsRef<Path>
+{
+    fs::read_to_string(path).unwrap()
+}
+
+fn input(day: usize) -> String {
+    let path = PathBuf::from(format!("{}/inputs/{:02}.txt", env!("CARGO_MANIFEST_DIR"), day));
+    load_file(path)
+}
