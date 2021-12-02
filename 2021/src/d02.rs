1b2bea0c32e16e4afeb64ca8385fa631c8fdd552
Rob Donnelly <rfdonnelly@gmail.com>
2021-12-01 21:49:40 -0800

    2021 d02p2

--- 2021/src/d02.rs
+++ 2021/src/d02.rs
@@ -77,8 +77,39 @@ fn d02p1(input: &str) -> i32 {
     p.x * p.y
 }
 
-fn d02p2(input: &str) -> usize {
-    0
+struct Tuple {
+    x: i32,
+    y: i32,
+    aim: i32,
+}
+
+impl Tuple {
+    fn new() -> Self {
+        Self {
+            x: 0,
+            y: 0,
+            aim: 0,
+        }
+    }
+}
+
+fn d02p2(input: &str) -> i32 {
+    let p = input
+        .lines()
+        .map(Instruction::from)
+        .fold(Tuple::new(), |mut acc, instr| {
+            match instr.direction {
+                Direction::Down => acc.aim += instr.magnitude,
+                Direction::Up => acc.aim -= instr.magnitude,
+                Direction::Forward => {
+                    acc.x += instr.magnitude;
+                    acc.y += acc.aim * instr.magnitude;
+                }
+            }
+            acc
+        });
+
+    p.x * p.y
 }
 
 #[cfg(test)]
@@ -99,6 +130,22 @@ mod tests {
 
         let input = super::input(2);
         assert_eq!(super::d02p1(&input), 1690020);
+    }
+
+    #[test]
+    fn d02p2() {
+        let input = indoc!{"
+            forward 5
+            down 5
+            forward 8
+            up 3
+            down 8
+            forward 2
+        "};
+        assert_eq!(super::d02p2(input), 900);
+
+        let input = super::input(2);
+        assert_eq!(super::d02p2(&input), 1408487760);
     }
 }
 
