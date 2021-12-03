d4a7cf94076a88d39176b930f172800ecfbabe5e
Rob Donnelly <rfdonnelly@gmail.com>
2021-12-03 11:33:51 -0800

    d03p2

--- 2021/src/d03.rs
+++ 2021/src/d03.rs
@@ -80,6 +80,22 @@ impl Value {
             .map(|(i, v)| v << (size - i - 1))
             .sum()
     }
+
+    fn from_usize(from: usize, size: usize) -> Self {
+        let bits = (0..size)
+            .rev()
+            .map(|bit_idx| {
+                let bit_value = get_bit(from, bit_idx);
+                match bit_value {
+                    0 => -1,
+                    1 => 1,
+                    _ => unreachable!(),
+                }
+            })
+            .collect::<Vec<i32>>();
+
+        Self { bits }
+    }
 }
 
 
@@ -103,10 +119,65 @@ fn p1(input: &str) -> usize {
     gamma_rate * epsilon_rate
 }
 
-fn p2(input: &str) -> i32 {
-    input
+fn find_rating(values: &[usize], size: usize, criteria: Criteria, equal_result: EqualResult) -> usize {
+    let mut values = values.to_vec();
+
+    for bit_idx in (0..size).rev() {
+        let criteria = values
+            .iter()
+            .map(|value| Value::from_usize(*value, size))
+            .reduce(|a, b| a + b)
+            .unwrap()
+            .to_usize(size, criteria, equal_result);
+
+        let criteria_bit_value = get_bit(criteria, bit_idx);
+        values = values
+            .into_iter()
+            .filter(|value| {
+                let bit_value = get_bit(*value, bit_idx);
+                bit_value == criteria_bit_value
+            })
+            .collect::<Vec<usize>>();
+
+        if values.len() == 1 { break; }
+    }
+
+    values[0]
+}
+
+fn get_bit(value: usize, bit_idx: usize) -> usize {
+    (value >> bit_idx) & 1
+}
+
+fn p2(input: &str) -> usize {
+    let size = input
         .lines()
-        .count() as i32
+        .next()
+        .unwrap()
+        .len();
+
+    let values = input
+        .lines()
+        .map(|s| usize::from_str_radix(s, 2).unwrap())
+        .collect::<Vec<usize>>();
+
+    let oxygen_generator_rating =
+        find_rating(
+            &values,
+            size,
+            Criteria::MostCommon,
+            EqualResult::Value(1)
+        );
+
+    let co2_scrubber_rating =
+        find_rating(
+            &values,
+            size,
+            Criteria::LeastCommon,
+            EqualResult::Value(0)
+        );
+
+    oxygen_generator_rating * co2_scrubber_rating
 }
 
 #[cfg(test)]
@@ -137,11 +208,25 @@ mod tests {
 
     #[test]
     fn p2() {
+        assert_eq!(super::Value::from_usize(22, 5), super::Value { bits: vec![1, -1, 1, 1, -1] });
+
         let input = indoc! {"
+            00100
+            11110
+            10110
+            10111
+            10101
+            01111
+            00111
+            11100
+            10000
+            11001
+            00010
+            01010
         "};
-        assert_eq!(super::p2(input), -1);
+        assert_eq!(super::p2(input), 230);
 
         let input = super::input(super::DAY);
-        assert_eq!(super::p2(&input), -1);
+        assert_eq!(super::p2(&input), 3277956);
     }
 }
