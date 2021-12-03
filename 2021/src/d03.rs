974242c9dffa09cddb2d8edfb3d236a3c51a5c6e
Rob Donnelly <rfdonnelly@gmail.com>
2021-12-02 21:58:54 -0800

    d03p1

--- 2021/src/d03.rs
+++ 2021/src/d03.rs
@@ -10,10 +10,74 @@ pub fn run() {
     println!("d{:02}p2: {}", DAY, p2(&input));
 }
 
-fn p1(input: &str) -> i32 {
-    input
+#[derive(Debug)]
+struct Value {
+    bits: Vec<i8>,
+}
+
+impl From<&str> for Value {
+    fn from(s: &str) -> Self {
+        let bits = s
+            .chars()
+            .map(|c| match c {
+                '0' => -1,
+                '1' => 1,
+                _ => panic!(),
+            })
+            .collect::<Vec<i8>>();
+
+        Self { bits }
+    }
+}
+
+impl Add for Value {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        let bits = self.bits
+            .iter()
+            .zip(other.bits.iter())
+            .map(|(a, b)| a + b)
+            .collect::<Vec<i8>>();
+
+        Self { bits }
+    }
+}
+
+impl Value {
+    fn to_usize(self, size: usize) -> usize {
+        self.bits
+            .iter()
+            .map(|b| match b {
+                x if x > &0 => 1,
+                x if x < &0 => 0,
+                _ => panic!(),
+            })
+            .enumerate()
+            .map(|(i, v)| v << (size - i - 1))
+            .sum()
+    }
+}
+
+
+fn p1(input: &str) -> usize {
+    let size = input
         .lines()
-        .count() as i32
+        .next()
+        .unwrap()
+        .len();
+
+    let gamma_rate = input
+        .lines()
+        .map(Value::from)
+        .reduce(|a, b| a + b)
+        .unwrap()
+        .to_usize(size);
+
+    let mask = (1 << size) - 1;
+    let epsilon_rate = !gamma_rate & mask;
+
+    gamma_rate * epsilon_rate
 }
 
 fn p2(input: &str) -> i32 {
@@ -29,11 +93,23 @@ mod tests {
     #[test]
     fn p1() {
         let input = indoc! {"
+            00100
+            11110
+            10110
+            10111
+            10101
+            01111
+            00111
+            11100
+            10000
+            11001
+            00010
+            01010
         "};
-        assert_eq!(super::p1(input), -1);
+        assert_eq!(super::p1(input), 198);
 
         let input = super::input(super::DAY);
-        assert_eq!(super::p1(&input), -1);
+        assert_eq!(super::p1(&input), 2498354);
     }
 
     #[test]
