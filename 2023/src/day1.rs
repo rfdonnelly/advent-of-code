2acd84bd853e34ae55ca39cf332906704214ae34
Rob Donnelly <rfdonnelly@gmail.com>
2023-11-30 21:10:10 -0800

    Boiler plate for 2023

---
+++ 2023/src/day1.rs
@@ -0,0 +1,32 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day1)]
+fn parse(input: &str) -> String {
+    todo!()
+}
+
+#[aoc(day1, part1)]
+fn part1(input: &str) -> String {
+    todo!()
+}
+
+#[aoc(day1, part2)]
+fn part2(input: &str) -> String {
+    todo!()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(part1(&parse("<EXAMPLE>")), "<RESULT>");
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+    }
+}
