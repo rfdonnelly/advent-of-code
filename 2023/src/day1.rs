5557f9c3de4cf57f528537cc4f47fb7191a8e685
Rob Donnelly <rfdonnelly@gmail.com>
2023-12-01 08:33:52 -0800

    2023:1:1

--- 2023/src/day1.rs
+++ 2023/src/day1.rs
@@ -1,17 +1,29 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day1)]
-fn parse(input: &str) -> String {
-    todo!()
+fn parse(input: &str) -> Vec<u32> {
+    input.lines().map(|line| {
+        let mut digits = line
+            .chars()
+            .filter_map(to_digit_base_10);
+        let first = digits.next().unwrap();
+        let last = digits.last().unwrap_or(first);
+        first * 10 + last
+    })
+    .collect()
+}
+
+fn to_digit_base_10(c: char) -> Option<u32> {
+    c.to_digit(10)
 }
 
 #[aoc(day1, part1)]
-fn part1(input: &str) -> String {
-    todo!()
+fn part1(input: &[u32]) -> u32 {
+    input.iter().sum()
 }
 
 #[aoc(day1, part2)]
-fn part2(input: &str) -> String {
+fn part2(input: &[u32]) -> String {
     todo!()
 }
 
@@ -19,14 +31,34 @@ fn part2(input: &str) -> String {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use indoc::indoc;
+
+    const INPUT: &str = indoc! {"
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+    "};
+
+    #[test]
+    fn test_parse() {
+        let expected = vec![
+            12,
+            38,
+            15,
+            77,
+        ];
+
+        assert_eq!(parse(INPUT), expected);
+    }
 
     #[test]
     fn part1_example() {
-        assert_eq!(part1(&parse("<EXAMPLE>")), "<RESULT>");
+        assert_eq!(part1(&parse(INPUT)), 142);
     }
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+        assert_eq!(part2(&parse(INPUT)), "<RESULT>");
     }
 }
--- 2023/src/lib.rs
+++ 2023/src/lib.rs
@@ -1,1 +1,3 @@
 mod day1;
+
+aoc_runner_derive::aoc_lib! { year = 2023 }
