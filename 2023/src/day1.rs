bb9de9c3a4cdf7637a88abd9af8feeb3ba28adfa
Rob Donnelly <rfdonnelly@gmail.com>
2023-12-01 09:53:26 -0800

    2023:1:2

--- 2023/src/day1.rs
+++ 2023/src/day1.rs
@@ -1,20 +1,75 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use std::collections::HashMap;
+use tap::Tap;
 
-#[aoc_generator(day1)]
-fn parse(input: &str) -> Vec<u32> {
-    input.lines().map(|line| {
-        let mut digits = line
-            .chars()
-            .filter_map(to_digit_base_10);
-        let first = digits.next().unwrap();
-        let last = digits.last().unwrap_or(first);
-        first * 10 + last
-    })
-    .collect()
+#[aoc_generator(day1, part1)]
+fn parse_p1(input: &str) -> Vec<u32> {
+    input
+        .lines()
+        .map(|line| {
+            let mut digits = line.chars().filter_map(char_to_digit);
+            let first = digits.next().unwrap();
+            let last = digits.last().unwrap_or(first);
+            first * 10 + last
+        })
+        .collect()
 }
 
-fn to_digit_base_10(c: char) -> Option<u32> {
+fn char_to_digit(c: char) -> Option<u32> {
     c.to_digit(10)
+}
+
+#[aoc_generator(day1, part2)]
+fn parse_p2(input: &str) -> Vec<u32> {
+    let map = {
+        let mut m = HashMap::new();
+        m.insert("one", 1);
+        m.insert("two", 2);
+        m.insert("three", 3);
+        m.insert("four", 4);
+        m.insert("five", 5);
+        m.insert("six", 6);
+        m.insert("seven", 7);
+        m.insert("eight", 8);
+        m.insert("nine", 9);
+        m
+    };
+
+    let strs: Vec<String> = map
+        .keys()
+        .map(|k| k.to_string())
+        .chain((0..=9).map(|i| i.to_string()))
+        .collect();
+
+    input
+        .lines()
+        .map(|line| {
+            let digits = strs
+                .iter()
+                .filter_map(|s| line.find(s).map(|i| (s, i)))
+                // NOTE: find only returns first occurrence.
+                // To account for subsequent occurrences, add rfind results too
+                .chain(
+                    strs.iter()
+                    .filter_map(|s| line.rfind(s).map(|i| (s, i)))
+                )
+                .collect::<Vec<_>>()
+                .tap_mut(|v| v.sort_by_key(|&(_, i)| i));
+            let digits = digits.iter().filter_map(|(s, _)| str_to_digit(s, &map)).collect::<Vec<_>>();
+
+            println!("{} {:?}", line, digits);
+            let mut digits = digits.iter();
+            let first = digits.next().unwrap();
+            let last = digits.last().unwrap_or(first);
+            first * 10 + last
+        })
+        .collect()
+}
+
+fn str_to_digit(s: &str, map: &HashMap<&str, u32>) -> Option<u32> {
+    map.get(s)
+        .copied()
+        .or_else(|| s.chars().next().unwrap().to_digit(10))
 }
 
 #[aoc(day1, part1)]
@@ -23,42 +78,53 @@ fn part1(input: &[u32]) -> u32 {
 }
 
 #[aoc(day1, part2)]
-fn part2(input: &[u32]) -> String {
-    todo!()
+fn part2(input: &[u32]) -> u32 {
+    input.iter().sum()
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use indoc::indoc;
 
-    const INPUT: &str = indoc! {"
+    const INPUT_P1: &str = indoc! {"
         1abc2
         pqr3stu8vwx
         a1b2c3d4e5f
         treb7uchet
     "};
 
-    #[test]
-    fn test_parse() {
-        let expected = vec![
-            12,
-            38,
-            15,
-            77,
-        ];
+    const INPUT_P2: &str = indoc! {"
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+    "};
 
-        assert_eq!(parse(INPUT), expected);
+    #[test]
+    fn test_parse_p1() {
+        let expected = vec![12, 38, 15, 77];
+
+        assert_eq!(parse_p1(INPUT_P1), expected);
+    }
+
+    #[test]
+    fn test_parse_p2() {
+        let expected = vec![29, 83, 13, 24, 42, 14, 76];
+
+        assert_eq!(parse_p2(INPUT_P2), expected);
     }
 
     #[test]
     fn part1_example() {
-        assert_eq!(part1(&parse(INPUT)), 142);
+        assert_eq!(part1(&parse_p1(INPUT_P1)), 142);
     }
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse(INPUT)), "<RESULT>");
+        assert_eq!(part2(&parse_p2(INPUT_P2)), 281);
     }
 }
