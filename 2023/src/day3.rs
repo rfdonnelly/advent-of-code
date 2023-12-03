551299c27064715f3a365e4e49782fe61b198abd
Rob Donnelly <rfdonnelly@gmail.com>
2023-12-02 23:16:47 -0800

    2023:3:1

--- 2023/src/day3.rs
+++ 2023/src/day3.rs
@@ -1,16 +1,97 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use regex::Regex;
+
+#[derive(Debug)]
+struct Number {
+    value: u64,
+    start: Point,
+    end: Point,
+}
+
+impl Number {
+    fn is_adjacent(&self, rhs: &Symbol) -> bool {
+        // NOTE: Assumes numbers are no more than 4 digits
+        self.start.is_adjacent(&rhs.start) || self.end.is_adjacent(&rhs.start)
+    }
+}
+
+#[derive(Debug)]
+struct Symbol {
+    value: char,
+    start: Point,
+}
+
+#[derive(Debug)]
+struct Schematic {
+    numbers: Vec<Number>,
+    symbols: Vec<Symbol>,
+}
+
+#[derive(Debug)]
+struct Point {
+    x: usize,
+    y: usize,
+}
+
+impl Point {
+    fn from_offset(offset: usize, cols: usize) -> Self {
+        let x = offset % cols;
+        let y = offset / cols;
+
+        Point { x, y }
+    }
+
+    fn is_adjacent(&self, rhs: &Self) -> bool {
+        let y_diff = (rhs.y as isize - self.y as isize).abs();
+        let x_diff = (rhs.x as isize - self.x as isize).abs();
+
+        y_diff <= 1 && x_diff <= 1
+    }
+}
+
 #[aoc_generator(day3)]
-fn parse(input: &str) -> String {
-    todo!()
+fn parse(input: &str) -> Schematic {
+    let cols = input.find('\n').unwrap() + 1;
+    dbg!(&cols);
+    let numbers = Regex::new(r"\d+").unwrap();
+    let symbols = Regex::new(r"[^\d\.\n]").unwrap();
+
+    let numbers = numbers
+        .find_iter(input)
+        .map(|m| Number {
+            value: m.as_str().parse().unwrap(),
+            start: Point::from_offset(m.start(), cols),
+            end: Point::from_offset(m.end() - 1, cols),
+        })
+        .collect();
+    let symbols = symbols
+        .find_iter(input)
+        .map(|m| Symbol {
+            value: m.as_str().chars().next().unwrap(),
+            start: Point::from_offset(m.start(), cols),
+        })
+        .collect();
+
+    Schematic { numbers, symbols }
 }
 
 #[aoc(day3, part1)]
-fn part1(input: &str) -> String {
-    todo!()
+fn part1(input: &Schematic) -> u64 {
+    input
+        .numbers
+        .iter()
+        .filter(|number| {
+            input
+                .symbols
+                .iter()
+                .any(|symbol| number.is_adjacent(symbol))
+        })
+        .map(|number| number.value)
+        .sum()
 }
 
 #[aoc(day3, part2)]
-fn part2(input: &str) -> String {
+fn part2(input: &Schematic) -> String {
     todo!()
 }
 
@@ -35,11 +116,11 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        assert_eq!(part1(&parse("<EXAMPLE>")), "<RESULT>");
+        assert_eq!(part1(&parse(INPUT)), 4361);
     }
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
+        // assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
     }
 }
