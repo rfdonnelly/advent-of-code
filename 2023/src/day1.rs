8939b88e0d489910a8cff556a03cde917e745dd8
Rob Donnelly <rfdonnelly@gmail.com>
2023-12-02 20:53:02 -0800

    perf: shorter strings first

--- 2023/src/day1.rs
+++ 2023/src/day1.rs
@@ -58,15 +58,6 @@ impl FromStr for Digit {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let digit =
         match s {
-            "one" => 1,
-            "two" => 2,
-            "three" => 3,
-            "four" => 4,
-            "five" => 5,
-            "six" => 6,
-            "seven" => 7,
-            "eight" => 8,
-            "nine" => 9,
             "0" => 0,
             "1" => 1,
             "2" => 2,
@@ -77,6 +68,15 @@ impl FromStr for Digit {
             "7" => 7,
             "8" => 8,
             "9" => 9,
+            "one" => 1,
+            "two" => 2,
+            "three" => 3,
+            "four" => 4,
+            "five" => 5,
+            "six" => 6,
+            "seven" => 7,
+            "eight" => 8,
+            "nine" => 9,
             _ => unreachable!(),
         };
         Ok(Self(digit))
