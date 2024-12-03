2beaae006d6221faeb4cfd96a98df87b3a8e8750
Rob Donnelly <rfdonnelly@gmail.com>
2024-12-02 22:33:34 -0800

    chore: cargo fmt

--- 2024/src/day2.rs
+++ 2024/src/day2.rs
@@ -28,11 +28,7 @@ impl FromStr for Report {
 
 impl Report {
     fn is_safe(levels: &[Number]) -> bool {
-        let diffs: Vec<_> = levels
-            .iter()
-            .tuple_windows()
-            .map(|(l, r)| r - l)
-            .collect();
+        let diffs: Vec<_> = levels.iter().tuple_windows().map(|(l, r)| r - l).collect();
 
         let all_increasing = diffs.iter().all(|&x| x > 0);
         let all_decreasing = diffs.iter().all(|&x| x < 0);
@@ -50,7 +46,13 @@ impl Report {
             true
         } else {
             for exclude_i in 0..self.levels.len() {
-                let levels: Vec<_> = self.levels.iter().enumerate().filter(|&(i, _)| i != exclude_i).map(|(_, x)| x.clone()).collect();
+                let levels: Vec<_> = self
+                    .levels
+                    .iter()
+                    .enumerate()
+                    .filter(|&(i, _)| i != exclude_i)
+                    .map(|(_, x)| x.clone())
+                    .collect();
                 if Report::is_safe(&levels) {
                     return true;
                 }
@@ -109,12 +111,24 @@ mod tests {
     #[test]
     fn test_parse_p1() {
         let expected = vec![
-            Report { levels: vec![7, 6, 4, 2, 1] },
-            Report { levels: vec![1, 2, 7, 8, 9] },
-            Report { levels: vec![9, 7, 6, 2, 1] },
-            Report { levels: vec![1, 3, 2, 4, 5] },
-            Report { levels: vec![8, 6, 4, 4, 1] },
-            Report { levels: vec![1, 3, 6, 7, 9] },
+            Report {
+                levels: vec![7, 6, 4, 2, 1],
+            },
+            Report {
+                levels: vec![1, 2, 7, 8, 9],
+            },
+            Report {
+                levels: vec![9, 7, 6, 2, 1],
+            },
+            Report {
+                levels: vec![1, 3, 2, 4, 5],
+            },
+            Report {
+                levels: vec![8, 6, 4, 4, 1],
+            },
+            Report {
+                levels: vec![1, 3, 6, 7, 9],
+            },
         ];
         assert_eq!(parse_p1(INPUT_P1), expected);
     }
@@ -129,4 +143,3 @@ mod tests {
         assert_eq!(part2(&parse_p2(INPUT_P2)), 4);
     }
 }
-
